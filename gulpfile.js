7a3aad40af4e25ef60aa12f1886c36870b352e80
Petr Stepanov <kisstherail@gmail.com>
2016-01-18 18:04:00 -0500

    Set up Bower and Gulp

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+// sudo npm install gulp-uglify gulp-rename gulp-less gulp-concat gulp-plumber gulp-cache-bust gulp-replace --save-dev
+
+// Required
+
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+    less = require('gulp-less'),
+    concat = require('gulp-concat'),
+    plumber = require('gulp-plumber'),
+    cachebust = require('gulp-cache-bust'),
+    replace = require('gulp-replace');
+
+// Scripts Task
+
+gulp.task('scripts', function(){
+    // Copy js source files from bower packages to js/vendor/ folder
+	gulp.src(['bower_components/bootstrap/dist/js/bootstrap.js',
+			  'bower_components/jquery/dist/jquery.js',
+		  ])
+        .pipe(plumber())                    // prevents breaking and has to go first here
+        .pipe(gulp.dest('js/vendor/'));
+
+    // Concat vendor javascript and our javascript and minify everything
+    gulp.src(['js/vendor/*.js',
+			  'js/main.js'
+		  ])
+        .pipe(plumber())                    // prevents breaking and has to go first here
+        .pipe(concat('selimlab.js'))
+		.pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('js/'));
+});
+
+// LESS Task
+
+gulp.task('styles', function(){
+    gulp.src('css/less/selimlab.less')
+        .pipe(plumber())                    // prevents breaking
+        .pipe(less())
+        .pipe(gulp.dest('css/'));
+});
+
+// CacheBust https://www.npmjs.com/package/gulp-cache-bust
+
+gulp.task('cache', function(){
+    gulp.src('./*.html')
+        .pipe(replace(/(\?t=(\d)*)*/g, ''))    // http://www.cheatography.com/davechild/cheat-sheets/regular-expressions/ https://regex101.com/#javascript
+	    .pipe(cachebust({type: 'timestamp'}))
+	    .pipe(gulp.dest('./'));
+});
+
+// Watch task (primarily for LESS)
+
+gulp.task('watch', function(){
+    gulp.watch('js/main.js', ['scripts', 'cache']);
+    gulp.watch('css/less/*.less', ['styles', 'cache']);
+});
+
+// Default task (calls other tasks)
+
+gulp.task('default', ['scripts', 'styles', 'watch', 'cache']);
--- include/scripts.php
+++ include/scripts.php
@@ -1,12 +1,10 @@
-        <script src="js/vendor/jquery-1.11.2.min.js"></script>
-        <script src="js/vendor/bootstrap.min.js"></script>
-        <script src="js/main.js"></script>
+        <script src="js/faselim.min.js"></script>
         <script>
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
             })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-            
+
             ga('create', 'UA-37279720-4', 'auto');
-            ga('send', 'pageview');            
+            ga('send', 'pageview');
         </script>
