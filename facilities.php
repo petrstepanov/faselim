3afc239265533f8f0b4c40a081f4b8a01e4fb66f
Petr Stepanov <kisstherail@gmail.com>
2016-01-19 14:42:00 -0500

    Icon Positioning Fix

--- facilities.php
+++ facilities.php
@@ -7,7 +7,7 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
-        <link rel="stylesheet" href="css/selimlab.css?t=1453232474016">
+        <link rel="stylesheet" href="css/selimlab.css?t=1453232509999">
         <link rel="shortcut icon" href="favicon.ico">
     </head>
     <body>
