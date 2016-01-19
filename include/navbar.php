aa73cdd5e8c4224400d718e27139911e6f6b7f56
Petr Stepanov <kisstherail@gmail.com>
2016-01-19 01:30:44 -0500

    New Responsive Header - CSS

--- include/navbar.php
+++ include/navbar.php
@@ -1,27 +1,26 @@
-        <div class="container">
-            <nav class="navbar navbar-default">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="#">SelimLab</a>
-                    </div>
-                    <div id="navbar" class="navbar-collapse collapse">
-                        <ul class="nav navbar-nav">
-                        </ul>
-                        <ul class="nav navbar-nav navbar-right">
-                            <li id="index"><a href="./">Welcome</a></li>
-                            <li id="research"><a href="./research">Research</a></li>
-                            <li id="facilities"><a href="./facilities">Facilities</a></li>
-                            <li id="publications"><a href="./publications">Publications</a></li>
-                            <li id="teaching"><a href="./teaching">Teaching</a></li>
-                            <li id="people"><a href="./people">People</a></li>
-                        </ul>
-                    </div>
+<div class="selim-navbar-container">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="selim-navbar">
+                    <a href="./" title="Go Home">
+                        <img class="visible-xs logo" src="img/logo-horizontal-smaller.png" alt="SelimLab Logo Horizontal"/>
+                        <img class="visible-sm logo" src="img/logo-vertical-smaller.png" alt="SelimLab Logo Vertical"/>
+                        <img class="visible-md visible-lg logo" src="img/logo-vertical.png" alt="SelimLab Logo Vertical Smaller"/>
+                    </a>
+                    <a class="toggle-menu js-toggle-menu" href="#">
+                        <svg class="icon"><use xlink:href="img/icons.svg#icon-menu"></use></svg>
+                    </a>
+                    <ul class="hidden-xs navigation">
+                        <li id="index"><a href="./">Welcome</a></li>
+                        <li id="research"><a href="./research">Research</a></li>
+                        <li id="facilities"><a href="./facilities">Facilities</a></li>
+                        <li id="publications"><a href="./publications">Publications</a></li>
+                        <li id="teaching"><a href="./teaching">Teaching</a></li>
+                        <li id="people"><a href="./people">People</a></li>
+                    </ul>
                 </div>
-            </nav>
+            </div>
         </div>
+    </div>
+</div>
--- research.php
+++ research.php
@@ -7,7 +7,7 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
-        <link rel="stylesheet" href="css/selimlab.css">
+        <link rel="stylesheet" href="css/selimlab.css?t=1453184767555">
         <link rel="shortcut icon" href="favicon.ico">        
     </head>
     <body>
