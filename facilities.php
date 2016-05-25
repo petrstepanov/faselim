26c005edf59bda058dce90975522d9b41b1c173d
Petr Stepanov <kisstherail@gmail.com>
2016-05-25 17:31:16 -0400

    Added Gallery

--- facilities.php
+++ facilities.php
@@ -7,7 +7,7 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
-        <link rel="stylesheet" href="css/selimlab.css?t=1464127170471">
+        <link rel="stylesheet" href="css/selimlab.css?t=1464211791802">
         <link rel="shortcut icon" href="favicon.ico">
     </head>
     <body>
@@ -25,51 +25,102 @@
         <div class="container font-size-controller">
             <div class="row">
                 <div class="col-xs-12">
-                    <h3>Synthesis Facilities</h3>
-                    <ul class="list-cards row" id="synthesis-facilities-grid" data-columns>
+                    <h3 id="synthesis-facilities">Synthesis Facilities</h3>
+                    <div class="spacer-1em"></div>
+                    <ul class="list-cards row chocolat-parent" data-chocolat-title="Synthesis Facilities" id="synthesis-facilities-grid" data-columns>
                         <li>
-                            <img src="img/facilities/thumbnails/clean-room.jpg" />
+                            <a class="chocolat-image" href="img/facilities/clean-room.jpg" title="Clean Room Class 100">
+                                <img src="img/facilities/thumbnails/clean-room.jpg" />
+                            </a>
                             <div class="body">
                                 <p>Clean Room Class 100</p>
                             </div>
                         </li>
                         <li>
-                            <img src="img/facilities/thumbnails/gas-mixing-panel.jpg" />
+                            <a class="chocolat-image" href="img/facilities/gas-mixing-panel.jpg" title="State of the art automatic gas mixing panel">
+                                <img src="img/facilities/thumbnails/gas-mixing-panel.jpg" />
+                            </a>
                             <div class="body">
                                 <p>State of the art automatic gas mixing panel</p>
                             </div>
                         </li>
                         <li>
-                            <img src="img/facilities/thumbnails/annealing-furnace.jpg" />
+                            <a class="chocolat-image" href="img/facilities/annealing-furnace.jpg" title="Nano-synthesis unit and annealing furnaces (up to 1700 C)">
+                                <img src="img/facilities/thumbnails/annealing-furnace.jpg" />
+                            </a>
                             <div class="body">
                                 <p>Nano-synthesis unit and annealing furnaces (up to 1700 C)</p>
                             </div>
                         </li>
                         <li>
-                            <img src="img/facilities/thumbnails/spin-processor.jpg" />
+                            <a class="chocolat-image" href="img/facilities/spin-processor.jpg" title="Thin and ultra-thin films spin processor">
+                                <img src="img/facilities/thumbnails/spin-processor.jpg" />
+                            </a>
                             <div class="body">
                                 <p>Thin and ultra-thin films spin processor</p>
                             </div>
                         </li>
                         <li>
-                            <img src="img/facilities/thumbnails/atomic-layer-deposition.jpg" />
+                            <a class="chocolat-image" href="img/facilities/atomic-layer-deposition.jpg" title="Atomic Layer Deposition and Metal Organic Chemical Vapour Deposition Systems">
+                                <img src="img/facilities/thumbnails/atomic-layer-deposition.jpg" />
+                            </a>
                             <div class="body">
                                 <p>Atomic Layer Deposition and Metal Organic Chemical Vapour Deposition Systems <span class="badge">under construction</span></p>
                             </div>
                         </li>
                     </ul>
-                    <h3>Characterization Facilities</h3>
+                    <h3 id="characterization-facilities">Characterization Facilities</h3>
                     <p>Our laboratory is a home of a wide variety of defect characterization techniques and advanced optical spectrometers including:</p>
-                    <ul class="spaced">
-                        <li>Positron lifetime spectrometer</li>
-                        <li>Digital Coincidence Doppler Broadening Positron Annihilation Spectrometer (first digital system in United States)</li>
-                        <li>Temperature-dependent Photo-Hall Effect System</li>
-                        <li>Magnetoresistance effect set up</li>
-                        <li>Thermo-luminescence spectrometer</li>
-                        <li>Heating cooling sample stage ( 77k-700k) with optical measurements capabilities</li>
-                        <li>UV-VIS-NIR spectrophotometer for optical absorption measurements</li>
-                        <li>Photoluminescence spectrometer</li>
-                        <li>X-ray luminescence based spectrometer for luminescence and scintillation measurements. <a href="http://scitation.aip.org/content/aip/journal/rsi/83/10/10.1063/1.4764772">We have developed this novel spectrometer</a>.</li>
+                    <div class="spacer-2em"></div>
+                    <ul class="list-cards row" id="characterization-facilities-grid" data-columns>
+                        <li>
+                            <img src="img/facilities/thumbnails/positron-lifetime-spectrometer.jpg" />
+                            <div class="body">
+                                <p>Positron lifetime spectrometer</p>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="img/facilities/thumbnails/positron-doppler-spectrometer.jpg" />
+                            <div class="body">
+                                <p>Digital Coincidence Doppler Broadening Positron Annihilation Spectrometer. First digital system in United States.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="img/facilities/thumbnails/hall-effect-system.jpg" />
+                            <div class="body">
+                                <p>Temperature-dependent Photo-Hall Effect System and Magnetoresistance effect set up</p>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="img/facilities/thumbnails/thermo-luminescence-spectrometer.jpg" />
+                            <div class="body">
+                                <p>Thermo-luminescence spectrometer</p>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="img/facilities/thumbnails/x-ray-luminescence-spectrometer.jpg" />
+                            <div class="body">
+                                <p>X-ray luminescence based spectrometer for luminescence and scintillation measurements.</p><p><a href="http://scitation.aip.org/content/aip/journal/rsi/83/10/10.1063/1.4764772">We developed this novel spectrometer</a>.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="img/facilities/thumbnails/heating-cooling-sample-stage.jpg" />
+                            <div class="body">
+                                <p>Heating cooling sample stage (77k-700k) with optical measurements capabilities</p>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="img/facilities/thumbnails/uv-vis-spectrometer.jpg" />
+                            <div class="body">
+                                <p>UV-VIS-NIR spectrophotometer for optical absorption measurements</p>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="img/facilities/thumbnails/photoluminescence-spectrometers.jpg" />
+                            <div class="body">
+                                <p>Photoluminescence spectrometers</p>
+                            </div>
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -82,6 +133,9 @@
         ?>
         <script>
             $('.nav #facilities, #facilities a').addClass('active');
+            $('.chocolat-parent').Chocolat({
+                // fullScreen: true
+            });
         </script>
     </body>
 </html>
--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,7 @@ gulp.task('scripts', function() {
     gulp.src(['./bower_components/jquery/dist/jquery.js',
             './bower_components/bootstrap/dist/js/bootstrap.js',
             './bower_components/salvattore/dist/salvattore.js',
+            './bower_components/chocolat/dist/js/jquery.chocolat.js',
             './js/jquery-easing-effects.js',
             './js/main.js'
         ])
